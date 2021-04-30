abb4a97df7b6dbe01745bd27def2a436c8b6b44c
IncredibleBoxman <78204182+IncredibleBoxman@users.noreply.github.com>
2021-04-30 11:05:53 -0500

    Boss platforms now despawn and respawn depending on boss phases

--- Cacee_NES_Adventure.c
+++ Cacee_NES_Adventure.c
@@ -47,6 +47,9 @@ extern const byte Win_Screen_rle[];
 extern const byte Boss_Screen_pal[16];
 extern const byte Boss_Screen_rle[];
 
+extern const byte Boss_Screen2_pal[16];
+extern const byte Boss_Screen2_rle[];
+
 
 
 // link the pattern table into CHR ROM
@@ -68,6 +71,10 @@ extern const byte Boss_Screen_rle[];
 //#link "Test_Screen3.s"
 
 //#link "Boss_screen.s"
+
+
+//#link "Boss_screen2.s"
+
 
 
 //#link "Game_Over_Screen.s"
@@ -291,8 +298,13 @@ byte boss_head;
 //boss see byte
 
 byte boss_see;
+// bool for boss phases
 bool boss_phase_1 = true;
 bool boss_phase_2 = true;
+
+// count for boss screens
+
+byte boss_screen = 0;
 
 // stars x and y coordinates
 
@@ -327,8 +339,11 @@ bool starOne = true;
 bool starTwo = true;
 bool starThree = true;
 bool onPlatform;
-
+// if our boss is left bool
 bool boss_left = false;
+
+bool boss1 = true;
+bool boss2 = false; 
 //score value
 byte score = 0;
 // lives value
@@ -750,7 +765,7 @@ void bossRoom()
   //famitone_init(my_lost_soul_danmaku_music_data);
   famitone_init(Shatterhand_music_data);
   music_play(0);
-  show_screen(Boss_Screen_pal, Boss_Screen_rle);
+  show_screen(Boss_Screen2_pal, Boss_Screen2_rle);
   level_four_platforms();
   create_thwomp(200, 120);
   startingSpace(); 
@@ -1146,6 +1161,22 @@ void main() {
       
       //draw our boss and move it with boss_dx
       boss_head = draw_boss(boss_dx);
+      if (boss1)
+      {
+        clear_platforms();
+        boss1 = false;
+      }
+      if(boss2)
+      {
+        clear_platforms();
+        boss2 = false;
+      }
+      if (boss1 && boss2)
+      {
+        clear_platforms();
+        boss1 = false;
+        boss2 = false; 
+      }
       
       // our boss uses a moving thwomp to attack! Depending on if 
       // thwomp sees us or not, changes the X. If does't see us then
@@ -1165,19 +1196,35 @@ void main() {
       //if our count is 7 then boss will take a break so you can shoot him.
       if (boss_count == 7)
     {
+      
       boss_dx = boss_dx;
-     
+      // phase 1 spawn platforms
+        if (!boss2)
+        {
+          level_four_platforms();
+          if (boss_screen == 0)
+          {
+            show_screen(Boss_Screen_pal, Boss_Screen_rle);
+            boss_screen = 1; 
+          }
+        }
       // phase 1 break
       if (score >= 10 && boss_phase_1)
       {
         boss_count = 0;
         boss_phase_1 = false;
+        boss2 = true;
+        show_screen(Boss_Screen2_pal, Boss_Screen2_rle);
+        boss_screen = 0;
       }
         //phase 2 break
       else if ( score>= 20 && boss_phase_2)
       {
+        show_screen(Boss_Screen2_pal, Boss_Screen2_rle);
         boss_count = 0;
         boss_phase_2 = false; 
+        boss2 = true; 
+        boss_screen = 0;
       }
     }
     else
